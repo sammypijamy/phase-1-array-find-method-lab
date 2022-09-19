c7e60083ea4c38066d7089e4718263e86c7b7fca
sammypijamy <samaragransky@outlook.com>
2022-09-19 15:47:38 -0700

    commit tree 71a05b82fe4169e439b01960601ff40ba38d1a78

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(records) {
+ const found = records.find(function (record){
+    return record.result === "W"
+  })
+  return found ? found.year : undefined 
+}
+
